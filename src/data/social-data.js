41d92c324b29dc7451ca729ea134aa4844572d71
Md. Nayeem <renayeem@gmail.com>
2023-12-22 17:21:08 +0600

    project setup done

---
+++ src/data/social-data.js
@@ -0,0 +1,29 @@
+
+const social_data = [
+  {
+    id:1,
+    link:'#',
+    icon:'fa-brands fa-facebook-f',
+    title:'Facebook'
+  },
+  {
+    id:2,
+    link:'#',
+    icon:'fa-brands fa-twitter',
+    title:'Twitter'
+  },
+  {
+    id:3,
+    link:'#',
+    icon:'fa-brands fa-linkedin-in',
+    title:'Linkedin'
+  },
+  {
+    id:4,
+    link:'https://vimeo.com/',
+    icon:'fa-brands fa-vimeo-v',
+    title:'Vimeo'
+  },
+]
+
+export default social_data;
